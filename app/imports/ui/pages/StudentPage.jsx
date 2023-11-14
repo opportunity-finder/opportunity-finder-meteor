621728b04009f1e291f79d743b90b0ae3cb7681a
Ethan Mamuad <ethantm@hawaii.edu>
2023-11-14 11:45:54 -1000

    Fix 2

--- app/imports/ui/pages/StudentPage.jsx
+++ app/imports/ui/pages/StudentPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 /* A simple static component to render some text for the landing page. */
 const StudentPage = () => (
-  <Container id="student page" fluid className="py-3 student-background">
+  <Container id="student page" fluid className="py-3">
     <Row className="align-middle text-center">
 
       <Col className="d-flex flex-column justify-content-center">
